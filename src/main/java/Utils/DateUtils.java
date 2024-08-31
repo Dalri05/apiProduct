793e9d325cc6a5aa593b4db1e9cc7d5719f0df39
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-08-31 20:36:56 -0300

    feat : formatador de data

--- src/main/java/Utils/DateUtils.java
+++ src/main/java/Utils/DateUtils.java
@@ -1,4 +1,12 @@
 package Utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
+    public static String formatDate(long dateInTimestamp) {
+        Date date = new Date(dateInTimestamp);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        return formatter.format(date);
+    }
 }
